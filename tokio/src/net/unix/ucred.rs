2c09321c0e50330cbde5db522795ab6c2296d8f5
Felix Moebius <felix.moebius@posteo.de>
2020-12-15 17:37:03 +0100

    Forgot cfg

--- tokio/src/net/unix/ucred.rs
+++ tokio/src/net/unix/ucred.rs
@@ -48,7 +48,7 @@ pub(crate) use self::impl_macos::get_peer_cred;
 #[cfg(any(target_os = "solaris", target_os = "illumos"))]
 pub(crate) use self::impl_solaris::get_peer_cred;
 
-#[cfg(any(target_os = "linux", target_os = "android"))]
+#[cfg(any(target_os = "linux", target_os = "android", target_os = "unikraft"))]
 pub(crate) mod impl_linux {
     use crate::net::unix::UnixStream;
 
